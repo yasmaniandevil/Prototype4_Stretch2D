8abae5ed8de6417fdbb2250e0efc384a5d88f845
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2023-10-02 21:08:42 -0400

    derrr

--- Prototype4_Stretch2D/Assets/Movement.cs
+++ Prototype4_Stretch2D/Assets/Movement.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+    public float speed;
+    private Vector3 targetPos;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveObject();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        // Check if the object has reached the target position
+        if (Vector3.Distance(transform.position, targetPos) < 0.1f)
+        {
+            // If reached, randomize a new target position
+           MoveObject();
+        }
+    }
+
+    public void MoveObject()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+
+        targetPos = new Vector3(randomX, randomY, transform.position.z);
     }
 }
